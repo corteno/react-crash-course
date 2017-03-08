bfb28748627eb46b93c8cb95a3e79f5928abd9d8
corteno <corten.mgs@gmail.com>
2017-03-08 19:56:46 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import uuid from 'uuid';
+import $ from 'jquery';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
+import Todos from './Components/Todos'
+import './App.css';
+
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            projects: [],
+            todos: []
+
+        }
+    }
+
+    getTodos() {
+        $.ajax({
+            url: 'https://jsonplaceholder.typicode.com/todos',
+            dataType: 'json',
+            cache: false,
+            success: function (data) {
+                this.setState({todos: data}, function () {
+                    console.log(this.state);
+                });
+            }.bind(this),
+            error: function (xhr, status, err) {
+                console.log(err);
+            }
+        });
+    }
+
+
+    getProjects() {
+        this.setState({
+            projects: [
+                {
+                    id: uuid.v4(),
+                    title: 'Business website',
+                    category: 'Web Design'
+                },
+                {
+                    id: uuid.v4(),
+                    title: 'Social App',
+                    category: 'Mobile Developement'
+                },
+                {
+                    id: uuid.v4(),
+                    title: 'Ecommerce Shopping Cart',
+                    category: 'Web Developement'
+                }
+            ]
+        });
+    }
+
+    componentWillMount() {
+        this.getProjects();
+        this.getTodos();
+    }
+
+    componentDidMount() {
+        this.getTodos();
+    }
+
+    handleAddProject(project) {
+        let projects = this.state.projects;
+        projects.push(project);
+        this.setState({projects: projects});
+
+    }
+
+    handleDeleteProject(id) {
+        let projects = this.state.projects;
+        let index = projects.findIndex(x => x.id === id);
+
+        projects.splice(index, 1);
+        this.setState({projects: projects});
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <AddProject addProject={this.handleAddProject.bind(this)}/>
+                <Projects onDelete={this.handleDeleteProject.bind(this)} projects={this.state.projects}/>
+                <hr />
+                <Todos todos={this.state.todos} />
+            </div>
+        );
+    }
+}
+
+export
+default
+App;
